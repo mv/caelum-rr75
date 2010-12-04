12075c6e1247369f52113e8e2119376a2c85af2c
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-04 10:14:59 -0200

    Generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LojaDeCarros::Application.config.secret_token = '9a2e580560f4d6ea5de002ad4b77289b988d97dfbd6c4447e07d3308461a15e8871a7083438383f634ddfae805a25dfd5e4e3e8acd55a42be5649bbe5dedc825'
