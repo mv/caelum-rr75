6d8bd8c632939546e0849ceb3bb629d6042fe40e
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-04 11:55:29 -0200

    Cucumber: passo 1

---
+++ features/step_definitions/carros_steps.rb
@@ -0,0 +1,18 @@
+
+Dado /^que estou na pagina inicial$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado /^preencho o campo "([^"]*)" com "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado /^aperto o botao "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Entao /^eu deveria ver "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
