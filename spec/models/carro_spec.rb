66ef47dcfce748425b091087bc7c0ed90f06f9e3
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-11 16:02:21 -0200

    Fixtures

--- spec/models/carro_spec.rb
+++ spec/models/carro_spec.rb
@@ -6,3 +6,24 @@ describe Carro do
   end
 end
 
+# mais exemplos
+
+fixtures :carros
+
+it "deveria usar fixtures" do
+  monza = carros(:monza)
+  monza.nome.should be      == 'Monza'
+  monza.descricao.should be == 'Monza vermelho 92'
+  monza.preco.should be     == '12000'
+
+  monza = carros(:fusca)
+  fusca.nome.should be      == 'Fusca'
+  fusca.descricao.should be == 'Fusca gelo 87'
+  fusca.preco.should be     == '9000'
+
+  tempra = carros(:tempra)
+  tempra.nome.should be      == 'Tempra'
+  tempra.descricao.should be == 'Tempra roxo 98'
+  tempra.preco.should be     == '19000'
+
+end
