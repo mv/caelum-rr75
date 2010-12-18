8fb3b1005ade531634456e68f92b732b8228b263
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2010-12-18 09:28:43 -0200

    Refatorando: "time_now" como plugin

---
+++ vendor/plugins/hora_certa/lib/hora_certa.rb
@@ -0,0 +1,15 @@
+# HoraCerta
+#
+
+
+class HoraCerta
+
+  def self.print
+    uri = 'http://developer.yahooapis.com/TimeService/V1/getTime'
+    resposta   = Restfulie.at(uri).get(:appid => 'YahooDemo')
+    yahoo_time = resposta['Result']['Timestamp'].to_i
+    Time.at(yahoo_time).strftime("%Y-%m-%d %H:%S")
+  end
+
+end
+
